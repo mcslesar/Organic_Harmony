31a6bb099a99b849f3341299f9c372df0bf5e900
Malygin Anton <malygin.anton@outlook.com>
2023-11-10 18:27:59 +0300

    accordeon

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,26 @@ const swiper = new Swiper('.bestseller__swiper', {
         type: 'bullets',
     },
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const accordions = document.querySelectorAll('.faq__item');
+    accordions.forEach(el => {
+        el.addEventListener('click', (e) => {
+            const self = e.currentTarget;
+            const control = self.querySelector('.faq__btn');
+            const content = self.querySelector('.faq__text');
+            self.classList.toggle('open');
+
+            if (self.classList.contains('open')) {
+                control.setAttribute('aria-expanded', true);
+                control.setAttribute('aria-hidden', false);
+                content.style.maxHeight = content.scrollHeight + 'px';
+            } else {
+                control.setAttribute('aria-expanded', false);
+                control.setAttribute('aria-hidden', true);
+                content.style.maxHeight = null;
+                
+            }
+        });
+    });
+});
